b522f6839883d204d7f0789e70afc42b8bcd7d58
edwardASavage <15561454@students.lincoln.ac.uk>
2019-12-05 16:24:58 +0000

    Finalised error handling for all functions. added comments.

--- AdvProgProto1/Driver.cpp
+++ AdvProgProto1/Driver.cpp
@@ -3,7 +3,6 @@
 #include "Rectangle.h"
 #include "Square.h"
 #include "Movable.h"
-#include <typeinfo>
 
 #include <iostream>
 #include <string>
@@ -21,6 +20,13 @@ int main()
 
 	while (userCommand.compare("exit") != 0)
 	{
+		cout << "Command list:\n"
+			<< "addR(int X,  int Y, float height, float width)\n"
+			<< "addS(int X, int Y, float edge)\n"
+			<< "addC(int X, int Y, float radius\n"
+			<< "move(int object-location, int new-X, int new-Y)\n"
+			<< "scale(int object-location, float scale-X, float scale-Y)\n"
+			<< "display()\n" << endl;
 
 		cout << "Enter the command: ";
 		getline(cin, userCommand);
@@ -37,70 +43,115 @@ int main()
 		}
 
 		string command;
+		// error handling for non valid cases.
+		
 		try
 		{
 			parameters.size() == 0 ? throw command : command = parameters[0];
+			if (command != "addR" && command!= "addS" && command != "addC" 
+				&& command != "scale" && command != "move" && command != "display")
+			{
+				throw command;
+			}
+
+			// alternatively, regex match case can be used to improve readability:
+
+			/*
+			#include <regex>
+
+			regex expression("(add[R,S,C])|(scale)|(move)|(display)");
+
+			if (!regex_match(command, expression))
+			{
+				throw command;
+			}
+			*/
+			
 		}
 		catch (string e)
 		{
-			command = e;
-			cout << "No compatable command entered" << endl;
+			cout << "No compatable command entered, please check command list. \n" << endl;
 		}
-		
-		// implement a string tokenizer to populate the parameters vector 
-		// check function strtok_s
-
-		// as a result of the process, parameters[0] should hold your command, followed by your parameters 
-		
 
 		int x, y;
-		// in the following code, consider checking for the arguments.
-		// in case of too few arguments, you may remind the user the correct format
-
 		if (command.compare("addR") == 0) {
-
-			if (parameters.size() == 5) {
-
+			try
+			{
+				parameters.size() != 5 ? throw command:
 				x = stoi(parameters[1].c_str());
 				y = stoi(parameters[2].c_str());
-				int h = stoi(parameters[3].c_str());
-				int w = stoi(parameters[4].c_str());
+				float h = stof(parameters[3].c_str());
+				float w = stof(parameters[4].c_str());
 				Rectangle* r = new Rectangle(x, y, h, w);
 				shapes.push_back(r);
-
 				cout << r;
 			}
+			catch (string e)
+			{
+				parameters.size() > 5 ? cout << "too many parameters entered." << endl
+					: cout << "too few parameters entered." << endl;
+				cout << "proper use: " << "addR(int X,  int Y, float height, float width)\n" << endl;
+			}
+
+				
 			
 			// cout overload here; 
 		}
 
 		else if (command.compare("addS") == 0) {
-			// get parameters
-			x = stoi(parameters[1].c_str());
-			y = stoi(parameters[2].c_str());
-			int e = stoi(parameters[3].c_str());
-			Square* s = new Square(x, y, e);
-			shapes.push_back(s);
-			cout << s;
+			try
+			{
+				parameters.size() != 4 ? throw command :
+				// get parameters
+				x = stoi(parameters[1].c_str());
+				y = stoi(parameters[2].c_str());
+				float e = stof(parameters[3].c_str());
+				Square* s = new Square(x, y, e);
+				shapes.push_back(s);
+				cout << s;
+			}
+			catch (string e)
+			{
+				parameters.size() > 4 ? cout << "too many parameters entered." << endl
+					: cout << "too few parameters entered." << endl;
+				cout << "proper use: " << "addS(int X, int Y, float edge)\n" << endl;
+			}
+			
 		}
 
 		if (command.compare("addC") == 0) {
-			// get parameters
-			x = stoi(parameters[1].c_str());
-			y = stoi(parameters[2].c_str());
-			int r = stoi(parameters[3].c_str());
-			Circle* c = new Circle(x, y, r);
-			shapes.push_back(c);
-			cout << c;
-
+			try
+			{
+				parameters.size() != 4 ? throw command :
+				x = stoi(parameters[1].c_str());
+				y = stoi(parameters[2].c_str());
+				float r = stoi(parameters[3].c_str());
+				Circle* c = new Circle(x, y, r);
+				shapes.push_back(c);
+				cout << c;
+			}
+			catch (string e)
+			{
+				parameters.size() > 4 ? cout << "too many parameters entered." << endl
+					: cout << "too few parameters entered." << endl;
+				cout << "proper use: " << "addC(int X, int Y, float radius\n" << endl;
+			}		
 		}
 
 		else if (command.compare("scale") == 0) {
 			int index = stoi(parameters[1].c_str());
 			float scaleX = stof(parameters[2].c_str());
 			float scaleY = stof(parameters[3].c_str());
-				Shape* s = (shapes[index - 1]);
 
+			try
+			{
+				if (index - 1 <= -1 || index - 1 > shapes.size())
+				{
+					throw 0;
+				}
+
+				Shape* s = (shapes[index - 1]);
+				// check the object type before applying scale.
 				if (dynamic_cast<Square*>(s) != NULL)
 				{
 					cout << "Scaling object Square...\n";
@@ -114,50 +165,79 @@ int main()
 					cout << "Scaling object Circle...\n";
 					dynamic_cast<Circle*>(s)->scale(scaleX, scaleY);
 				}
-
-				// check what type of shape, then scale
-
-				s->toString();
+				// output scaled object using 
+				cout << s;
+				// delete pointer.
+				delete s;
 			}
+			catch (int e)
+			{
+				cout << "scale function used was out of bounds! \n"
+					<< "proper use: " 
+					<< "scale(int object-location, float scale-X, float scale-Y)\n" << endl;
+			}
+		}
 			
 
 		
 
 		else if (command.compare("move") == 0) {
 			// move object at index 
-			int shapeNo = stoi(parameters[1].c_str());
-			int newX = stoi(parameters[2].c_str());
-			int newY = stoi(parameters[3].c_str());
-			// you may want to check if the index exists or not!
+			
+			
+			try
+			{
+				if (parameters.size() != 4)
+				{
+					parameters.size() > 4 ? cout << "too many parameters entered." << endl
+						: cout << "too few parameters entered." << endl;
+					throw 0;
+				}
 
-			// Study the following code. A Shape object is not Movable, but all derived classes are...
-			// you can't automatically type cast from a Shape to a Movable, but you can force a downcasting
-			Movable* m = dynamic_cast<Movable*>(shapes[shapeNo - 1]);
+				int shapeNo = stoi(parameters[1].c_str());
+				int newX = stoi(parameters[2].c_str());
+				int newY = stoi(parameters[3].c_str());
 
-			m->move(newX, newY);
 
-			shapes[shapeNo - 1]->toString();
+				if (shapeNo-1 <= -1 || shapeNo-1 > shapes.size())
+				{
+					cout << "object location invalid! \n" << endl;
+					throw 0;
+				}
+
+				Movable* m = dynamic_cast<Movable*>(shapes[shapeNo - 1]);
+				
+				m->move(newX, newY);
+
+				shapes[shapeNo - 1]->toString();
+
+			}
+			catch (int e)
+			{
+				cout << "Invalid use of move function! \n";
+			}
+
+			
 		}
 		else if (command.compare("display") == 0) {
+
+			if (shapes.size() == 0)
+			{
+				cout << "No shapes to display! \n" << endl;
+			}
 
 			for (int i = 0; i != shapes.size(); i++)
 			{
 				shapes[i]->toString();
 			}
-			// this is not given in our example, but why don't you implement a display function which shows all objects stored in shapes?
 		}
-		/*
-		// do any necessary postprocessing at the end of each loop...
-		// yes, there is some necessary postprocessing...
-		cout << endl << endl;
-		*/
 		parameters.clear();
 	}
 	
-	for (int i = 0; i != shapes.size(); i++)
-	{
-		delete shapes[i];
-	}
+	
+
+
+
 
 	cout << "Press any key to continue...";
 	std::getchar();
